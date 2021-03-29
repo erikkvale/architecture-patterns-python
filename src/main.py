91d479791eb8c08752126afb55cb69a35dbe2706
Erik Kvale <eirikval@gmail.com>
2021-03-28 23:26:20 -0500

    Added deallocation method and supporting logic.

--- src/main.py
+++ src/main.py
@@ -19,14 +19,28 @@ class Batch:
     def __init__(self, reference_id: str, sku: str, quantity: int, eta: Optional[date]):
         self.reference_id = reference_id
         self.sku = sku
-        self.available_quantity = quantity
+        self._purchased_quantity = quantity
         self.eta = eta
+        self._allocations = set()
 
     def __len__(self):
         return self.available_quantity
 
     def allocate(self, order_line: OrderLine):
-        self.available_quantity -= order_line.quantity
-        
+        if self.can_allocate(order_line):
+            self._allocations.add(order_line)
 
+    def deallocate(self, order_line: OrderLine):
+        if order_line in self._allocations:
+            self._allocations.remove(order_line)
 
+    @property
+    def allocated_quantity(self) -> int:
+        return sum(line.quantity for line in self._allocations)
+
+    @property
+    def available_quantity(self) -> int:
+        return self._purchased_quantity - self.allocated_quantity
+
+    def can_allocate(self, order_line: OrderLine) -> bool:
+        return self.sku == order_line.sku and self.available_quantity >= order_line.quantity
